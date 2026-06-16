c1398b61b75b409a039d81ebdb8f360a4a77a02b
Agung Setio Budi <agungsetiobudi19@gmail.com>
2026-06-16 10:10:23 +0800

    comment protocolseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ProtocolSeeder::class,
+            //ProtocolSeeder::class,
             UserSeeder::class
         ]);
 
